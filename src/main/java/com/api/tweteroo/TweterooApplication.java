cf2443f7c09447282aa442d9f32412e0de150c69
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-03-30 20:29:30 -0300

    first commit

---
+++ src/main/java/com/api/tweteroo/TweterooApplication.java
@@ -0,0 +1,12 @@
+package com.api.tweteroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweterooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweterooApplication.class, args);
+	}
+}
